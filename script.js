2d026aedd66efc13e104d9161604080197bddd70
GabrielAndrade <91852179+Gabriel-A-B@users.noreply.github.com>
2024-04-24 08:54:59 -0300

    update

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
+const videoContainer = document.querySelector(".videos__container")
+
 const api = fetch("http://localhost:3000/videos")
-.then(res => console.log(res.json()))
+.then(res => res.json())
+.then((videos) =>
+    videos.forEach((video) => {
+        videoContainer.innerHTML += `
+        <li class='videos__item'>
+        <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+            <div class="descricao-video">
+                <img class="img-canal" src="${video.imagem}" alt="Logo do canal"></img>
+                <h3 class="titulo-video">${video.titulo}</h3>
+                <p class="descricao-video">${video.descricao}</p>
+            </div>
+        </li> 
+        `;
+    })
+)
+.catch((error) => {
+    videoContainer.innerHTML = `
+    <p> ocorreu um erro: "${error}",Conserte!!
+    `
+})
