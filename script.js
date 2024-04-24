aeef3db449b605989ff07a5b45be0be91be2837e
GabrielAndrade <91852179+Gabriel-A-B@users.noreply.github.com>
2024-04-24 09:29:55 -0300

    up

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-const videoContainer = document.querySelector(".videos__container")
+const containerVideos = document.querySelector(".videos__container");
 
 const api = fetch("http://localhost:3000/videos")
 .then(res => res.json())
 .then((videos) =>
-    videos.forEach((video) => {
-        videoContainer.innerHTML += `
-        <li class='videos__item'>
-        <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+    videos.forEach((video)=> {
+        containerVideos.innerHTML += `
+        <li class="videos__item">
+            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
             <div class="descricao-video">
-                <img class="img-canal" src="${video.imagem}" alt="Logo do canal"></img>
+                <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
                 <h3 class="titulo-video">${video.titulo}</h3>
-                <p class="descricao-video">${video.descricao}</p>
+                <p class="titulo-canal">${video.descricao}</p>
             </div>
-        </li> 
+        </li>
         `;
     })
 )
 .catch((error) => {
-    videoContainer.innerHTML = `
-    <p> ocorreu um erro: "${error}",Conserte!!
-    `
+    containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error} </p>`;
 })
